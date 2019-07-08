623c0bac84966c1ef425332cad1528cd8b0fa634
root <root@yonglin.emd.sashq-r.openstack.sas.com>
2019-07-08 18:07:31 +0000

    0708

--- benchmark/autosklearn/bank_classification.py
+++ benchmark/autosklearn/bank_classification.py
@@ -13,47 +13,49 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn import preprocessing
+##################################################
+from DateTime import DateTime
+import time
+
+current_time = DateTime(time.time(), 'US/Eastern')
+framework = 'autosklearn'
+datasetn = 'bankmarketing'
+foldn =  '3'
+
+
+resultfile = framework + datasetn + foldn + \
+current_time.year() + current_time.amonth()+current_time.day() + \
+current_time.h_24() + current_time.mintue()  + time.time()+'.txt'
+
+
 data =pd.read_csv("/home/test/bank.csv",delimiter=';')
-
-numeric_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='median')),\
-    ('scaler', StandardScaler())])
-
-numeric_features = ['age','balance','day','duration','pdays','previous']
-numeric_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='median')),
-    ('scaler', StandardScaler())])
-
-
-categorical_features = ['job', 'marital', 'education', 'default','housing', 'loan', 'contact', 'month', 'campaign', 'poutcome']
-
-
-categorical_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='constant', fill_value='missing')),\
-    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
-
-preprocessor = ColumnTransformer(transformers=[('num', numeric_transformer, numeric_features),\
-     ('cat', categorical_transformer, categorical_features)])
-
-
 column= data.columns.values
 X = data[column[:-1]]
 y = data[column[-1]]
 lb = preprocessing.LabelBinarizer()
 y= lb.fit_transform(y)
-print(y[:50])
+##################################################
+numeric_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='median')),\
+    ('scaler', StandardScaler())])
+numeric_features = ['age','balance','day','duration','pdays','previous']
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())])
+categorical_features = ['job', 'marital', 'education', 'default','housing', 'loan', 'contact', 'month', 'campaign', 'poutcome']
+categorical_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='constant', fill_value='missing')),\
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+preprocessor = ColumnTransformer(transformers=[('num', numeric_transformer, numeric_features),\
+     ('cat', categorical_transformer, categorical_features)])
+################################################################################
 automl = autosklearn.classification.AutoSklearnClassifier()
-
 clf = Pipeline(steps=[('preprocessor', preprocessor),
                       ('classifier', automl)])
-
 X_train, X_test, y_train, y_test = \
 sklearn.model_selection.train_test_split(X, y, random_state=1)
 
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
 clf.fit(X_train, y_train)
-print("model score: %.3f" % clf.score(X_test, y_test))
 
+#pattern = r"(?P<framework>[\w\-]+?)_(?P<task>[\w\-]+)_(?P<fold>\d+)(_(?P<datetime>\d{8}T\d{6}))?.csv"
+
+print("model score: %.3f" % clf.score(X_test, y_test))
 print(automl.show_models())
-#automl.fit(X_train, y_train)
-#y_hat = automl.predict(X_test)
-#print("Accuracy score", sklearn.metrics.accuracy_score(y_test, y_hat))
