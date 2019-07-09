419b973cd4061be341709a6ea3434c024a741d80
root <root@yonglin.emd.sashq-r.openstack.sas.com>
2019-07-09 17:34:14 +0000

    replicate bankmarketing tpot

--- benchmark/tpot/rep/bankmarking3f.py
+++ benchmark/tpot/rep/bankmarking3f.py
@@ -27,19 +27,62 @@ openml.org/t/14965,bank-marketing,TPOT,3,0.934107,local,0.9.6,,stable,2019-07-08
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import load_iris
+from sklearn import preprocessing
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.compose import ColumnTransformer
 from xgboost import XGBClassifier
-import xgboost
+import xgboost as xgb
 import sklearn
+###### Read in data
+import pandas as pd
+import numpy as np
+import pickle
+from DateTime import DateTime
+import time
 
-print('sklearn version: %s' % sklearn.__version__)
-print('xgboost version: %s' % xgboost.__version__)
+current_time = DateTime(time.time(), 'US/Eastern')
+framework = 'tpot'
+datasetn = 'bankmarketing'
+foldn =  '3'
+dirt = '../'
 
-X, y = load_iris(return_X_y=True)
-from sklearn.model_selection import train_test_split 
+resultfile = str(framework) + str(datasetn) + str(foldn) + \
+str(current_time.year()) + str(current_time.aMonth())+ str(current_time.day()) + \
+str(current_time.h_24()) + str(current_time.minute())  + str(time.time())+'.txt'
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35, random_state=143)
-# Without using the pipeline: 
-xgb = XGBClassifier()
+
+
+data =pd.read_csv("/home/test/bank.csv",delimiter=';')
+column= data.columns.values
+X = data[column[:-1]]
+y = data[column[-1]]
+lb = preprocessing.LabelBinarizer()
+y= lb.fit_transform(y)
+##################################################
+numeric_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='median')),\
+    ('scaler', StandardScaler())])
+numeric_features = ['age','balance','day','duration','pdays','previous']
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())])
+categorical_features = ['job', 'marital', 'education', 'default','housing', 'loan', 'contact', 'month', 'campaign', 'poutcome']
+categorical_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='constant', fill_value='missing')),\
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+preprocessor = ColumnTransformer(transformers=[('num', numeric_transformer, numeric_features),\
+     ('cat', categorical_transformer, categorical_features)])
+################################################################################
+X_train, X_test, y_train, y_test = \
+sklearn.model_selection.train_test_split(X, y, random_state=1)
+
+
+def save_object(obj, filename):
+    with open(filename, 'wb') as output:  # Overwrites any existing file.
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+
 xgb.fit(X_train, y_train, eval_metric='auc')  # works fine
 
 # Making a pipeline with this classifier and a scaler:
