8a456b7b2f9ba5f526cb8d7378dd9912a56aafe4
yonglinroot <zhuygln@gmail.com>
2019-07-19 19:04:17 +0000

    07192019

--- benchmark/autosklearn/bank_classification.py
+++ benchmark/autosklearn/bank_classification.py
@@ -36,81 +36,64 @@ dtrain = pd.read_csv(dirt+dataset+"train.csv")
 dvalid =pd.read_csv(dirt+dataset+"valid.csv")
 dtest =pd.read_csv(dirt+dataset+"test.csv")
 
-
+ctrain=  dtrain.append(dvalid)
 data = pd.read_csv(dirt+dataset+".csv")
 
-data_train = dtrain.append(dvalid)
+#data_train = dtrain.append(dvalid)
+#dtest = dtest.drop(['_dmIndex_','_PartInd_'],axis=1)
+#data_train = data_train.drop(['_dmIndex_','_PartInd_'],axis=1)
 
-dtest = dtest.drop(['_dmIndex_','_PartInd_'],axis=1)
-data_train = data_train.drop(['_dmIndex_','_PartInd_'],axis=1)
-
-column= data_train.columns.values
-X_test = dtest[column[:-1]]
-y_test = dtest[column[-1]]
+column= data.columns.values
+index = list(column[-2:])
+print(index)
+print(data[column[-2:]])
 ##################################################
-
-print(column)
-numeric_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='median')),\
-    ('scaler', StandardScaler())])
-
 numeric_features = ['age','duration','pdays','previous','emp_var_rate','cons_price_idx','cons_conf_idx','euribor3m','nr_employed']
-numeric_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='median')),
-    ('scaler', StandardScaler())])
 categorical_features = ['job', 'marital', 'education', 'default','housing', 'loan', 'contact', 'month','day_of_week', 'campaign', 'poutcome']
-categorical_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='constant', fill_value='missing')),\
-    ('onehot', OneHotEncoder())])
-preprocessor = ColumnTransformer(transformers=[('num', numeric_transformer, numeric_features),\
-     ('cat', categorical_transformer, categorical_features)])
-lb = preprocessing.LabelBinarizer()
-################################################################################
-X = pd.DataFrame(data.drop(['y'],axis=1))
 data["y"]=data.where(data["y"]=='yes',1)
 data["y"]=data.where(data["y"]=='no',0)
-#for cf in categorical_features:
-#   labels,uniques= pd.factorize(data[cf])
-#   data[cf]=labels
-#
-#print(data[categorical_features])
-##y = lb.fit_transform(y)
+################################################################################
+data_num = data[numeric_features]
+data_num = data_num.fillna(data_num.mean())
+data_num = pd.DataFrame(StandardScaler().fit_transform(data_num))
+##### ONEHOTENCODING in scikit learn do not keep column names, so use pandas.get_dummies
+data_cat = data[categorical_features].fillna('missing')
+data_cat=pd.get_dummies(data_cat)
 
-#y_test = lb.fit_transform(y_test)
-print("before",X)
+X = pd.concat([data[index],data_num,data_cat], axis=1)
 
-#X_train = preprocessor.fit_transform(X_train)
-X = preprocessor.fit_transform(X)
-print("after,",X)
-print(OneHotEncoder.feature_indices_)
-print("\n\n")
+y = data[index+['y']]
 
-X_train =X["_PartInd_"]>0
+X_train =X[X['_PartInd_']>0]
+X_test =X[X['_PartInd_']==0]
+y_test =y[y['_PartInd_']==0]
+y_train =y[y['_PartInd_']>0]
 
-X_test =X["_PartInd_"]==0
-X_test = X_train.drop(['_dmIndex_','_PartInd_'],axis=1)
-X_train = X_train.drop(['_dmIndex_','_PartInd_'],axis=1)
-y = data['y','_dmIndex_','_PartInd_']
-print(y)
+y_test = y_test.drop(columns=['_dmIndex_','_PartInd_']).astype(int)
+y_train =y_train.drop(columns=['_dmIndex_','_PartInd_']).astype(int)
+X_test = X_test.drop(columns=['_dmIndex_','_PartInd_'])
+X_train =X_train.drop(columns=['_dmIndex_','_PartInd_'])
+
+
 
 #######################################################################################
 automl = autosklearn.classification.AutoSklearnClassifier(time_left_for_this_task=timeforjob,\
+        per_run_time_limit=int(timeforjob/10),\
         delete_tmp_folder_after_terminate=False,\
-        n_jobs=4,\
+        ensemble_memory_limit=10240,
+        seed=1,
+        ml_memory_limit=30720,
+        n_jobs=8,\
         resampling_strategy='cv',\
         resampling_strategy_arguments={'folds': int(foldn)},)
 
 ##########################################
-
 #clf = Pipeline(steps=[('preprocessor', preprocessor),
-print(np.ndim(X_train[0]))
 automl.fit(X_train, y_train)
-print(np.ndim(X_train[0]))
 automl.refit(X_train, y_train)
-print(np.ndim(X_train[0]))
-print(np.ndim(X_test[0]))
 y_pred = automl.predict(X_test)
 print("accuracy: ", sklearn.metrics.accuracy_score(y_test, y_pred))
 print(automl.sprint_statistics())
-print(automl.show_models())
 #pattern = r"(?P<framework>[\w\-]+?)_(?P<task>[\w\-]+)_(?P<fold>\d+)(_(?P<datetime>\d{8}T\d{6}))?.csv"
 
 def save_object(obj, filename):
@@ -122,13 +105,10 @@ save_object(automl.show_models(),str("showmodels")+resultfile)
 #results = cross_val_score(automl, X_train, y_train, cv=kfold)
 #print("Accuracy: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
 print("automodel1",automl.show_models())
-print(type(automl.show_models()))
-#print(automl.cv_results_)
 print("automl.fit")
-print(automl.fit(X_test, y_test))
 finalmodel_file ='finalmodelemsenble.pkl'
 finalmodel = open(finalmodel_file,'wb')
 pickle.dump((automl,X_train,y_train),finalmodel)
 finalmodel.close()
 
-#save_object(automl.cv_results_,str("cv_results")+resultfile)
+print(automl.cv_results_)
